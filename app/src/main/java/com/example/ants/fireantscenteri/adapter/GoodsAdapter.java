d2150cdab5127eaa83a5497ae2b734ea3ff7ff6e
FireAnts <MyFireAnts@163.com>
2016-10-19 12:59:00 +0800

    添加的商品的adapter基本框架

---
+++ app/src/main/java/com/example/ants/fireantscenteri/adapter/GoodsAdapter.java
@@ -0,0 +1,97 @@
+package com.example.ants.fireantscenteri.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.ants.fireantscenteri.I;
+import com.example.ants.fireantscenteri.R;
+import com.example.ants.fireantscenteri.bean.NewGoodsBean;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by 15291 on 2016/10/19.
+ */
+
+public class GoodsAdapter extends RecyclerView.Adapter {
+
+    Context context;
+    List<NewGoodsBean> beanList;
+
+    public GoodsAdapter(Context context, List<NewGoodsBean> beanList) {
+        this.context = context;
+        this.beanList = beanList;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new FooterViewHolder(View.inflate(context, R.layout.item_footer, null));
+        } else {
+            holder = new GoodsViewHolder(View.inflate(context, R.layout.item_goods, null));
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == I.TYPE_FOOTER) {
+
+        } else {
+            GoodsViewHolder goodsViewHolder=(GoodsViewHolder)holder;
+            NewGoodsBean goodsBean = beanList.get(position);
+            goodsViewHolder.tvGoodsName.setText(goodsBean.getGoodsName());
+            goodsViewHolder.tvGoodsPrice.setText(goodsBean.getCurrencyPrice());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return beanList != null ? beanList.size() + 1 : 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        } else {
+            return I.TYPE_ITEM;
+        }
+    }
+
+    static class FooterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tvFooter)
+        TextView tvFooter;
+
+        FooterViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    static class GoodsViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivGoodsThumb)
+        ImageView ivGoodsThumb;
+        @BindView(R.id.tvGoodsName)
+        TextView tvGoodsName;
+        @BindView(R.id.tvGoodsPrice)
+        TextView tvGoodsPrice;
+        @BindView(R.id.layout_goods)
+        LinearLayout layoutGoods;
+
+        GoodsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
