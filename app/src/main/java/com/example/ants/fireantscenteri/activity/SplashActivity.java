54baa6be5fe5e0e7f54cd414c8f3313fba16557e
FireAnts <MyFireAnts@163.com>
2016-10-19 11:02:51 +0800

    添加butterknife的依赖

--- app/src/main/java/com/example/ants/fireantscenteri/activity/SplashActivity.java
+++ app/src/main/java/com/example/ants/fireantscenteri/activity/SplashActivity.java
@@ -20,6 +20,15 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        /**
+         * final boolean
+         * postDelayed(Runnable r, long delayMillis)
+         * Causes the Runnable r to be added to the message queue,
+         * to be run after the specified amount of time elapses.
+         * 导致要添加到消息队列中的可运行R，经过规定的时间量之后运行。
+         * 可运行将向此处理程序附加的线程上运行。 时基是uptimeMillis()，
+         * 时间在深睡眠中度过会增加一个额外的延迟执行。
+         */
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
