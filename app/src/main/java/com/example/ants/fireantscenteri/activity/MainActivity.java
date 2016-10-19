459c6a29ff97bb653801a1765fefe4efb6777b66
FireAnts <MyFireAnts@163.com>
2016-10-19 11:17:34 +0800

    添加菜单栏

--- app/src/main/java/com/example/ants/fireantscenteri/activity/MainActivity.java
+++ app/src/main/java/com/example/ants/fireantscenteri/activity/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.ants.fireantscenteri.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.ants.fireantscenteri.R;
 
@@ -11,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckedChange(View view) {
+
     }
 }
