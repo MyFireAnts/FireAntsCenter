69552774b3417e356cc7e2cf6deb0b4a4d2779fb
FireAnts <MyFireAnts@163.com>
2016-10-19 17:37:50 +0800

    商品详情获取数据并显示

--- app/src/main/java/com/example/ants/fireantscenteri/activity/GoodsDetailActivity.java
+++ app/src/main/java/com/example/ants/fireantscenteri/activity/GoodsDetailActivity.java
@@ -2,18 +2,96 @@ package com.example.ants.fireantscenteri.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.ants.fireantscenteri.I;
 import com.example.ants.fireantscenteri.R;
+import com.example.ants.fireantscenteri.bean.GoodsDetailsBean;
+import com.example.ants.fireantscenteri.net.NetDao;
+import com.example.ants.fireantscenteri.net.OkHttpUtils;
+import com.example.ants.fireantscenteri.utils.CommonUtils;
 import com.example.ants.fireantscenteri.utils.L;
+import com.example.ants.fireantscenteri.view.FlowIndicator;
+import com.example.ants.fireantscenteri.view.SlideAutoLoopView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class GoodsDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.backClickArea)
+    LinearLayout mBackClickArea;
+    @BindView(R.id.tv_good_name_english)
+    TextView mTvGoodNameEnglish;
+    @BindView(R.id.tv_good_name)
+    TextView mTvGoodName;
+    @BindView(R.id.tv_good_price_shop)
+    TextView mTvGoodPriceShop;
+    @BindView(R.id.tv_good_price_current)
+    TextView mTvGoodPriceCurrent;
+    @BindView(R.id.salv)
+    SlideAutoLoopView mSalv;
+    @BindView(R.id.indicator)
+    FlowIndicator mIndicator;
+    @BindView(R.id.wv_good_brief)
+    WebView mWvGoodBrief;
+
+    int goodsId;
+    GoodsDetailActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
-        int goodsID = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
-        L.e("details", "goodsid=" + goodsID);
+        ButterKnife.bind(this);
+        goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
+        L.e("details", "goodsid=" + goodsId);
+        if (goodsId == 0) {
+            finish();
+        }
+        mContext = this;
+        initView();
+        initData();
+        setListener();
+    }
+
+    private void setListener() {
+
+    }
+
+    private void initData() {
+        NetDao.downloadGoodsDetail(mContext, goodsId, new OkHttpUtils.OnCompleteListener<GoodsDetailsBean>() {
+            @Override
+            public void onSuccess(GoodsDetailsBean result) {
+                L.i("details=" + result);
+                if (result != null) {
+                    showGoodDetails(result);
+                } else {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                finish();
+                L.e("details,error=" + error);
+                CommonUtils.showShortToast(error);
+            }
+        });
+    }
+
+    private void showGoodDetails(GoodsDetailsBean details) {
+        mTvGoodNameEnglish.setText(details.getGoodsEnglishName());
+        mTvGoodName.setText(details.getGoodsName());
+        mTvGoodPriceCurrent.setText(details.getCurrencyPrice());
+        mTvGoodPriceShop.setText(details.getShopPrice());
+
+    }
+
+    private void initView() {
+
     }
 }
