87f115fc6c11548836678e19e36f8fc62428e8d3
FireAnts <MyFireAnts@163.com>
2016-10-19 13:23:38 +0800

    添加新品下载的网络请求的方法

---
+++ app/src/main/java/com/example/ants/fireantscenteri/net/NetDao.java
@@ -0,0 +1,32 @@
+package com.example.ants.fireantscenteri.net;
+
+import android.content.Context;
+
+import com.example.ants.fireantscenteri.I;
+import com.example.ants.fireantscenteri.bean.GoodsDetailsBean;
+import com.example.ants.fireantscenteri.bean.NewGoodsBean;
+
+
+/**
+ * Created by clawpo on 2016/10/17.
+ */
+
+public class NetDao {
+    public static void downloadNewGoods(Context context, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean[]> listener) {
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID, String.valueOf(I.CAT_ID))
+                .addParam(I.PAGE_ID, String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE, String.valueOf(I.PAGE_SIZE_DEFAULT))
+                .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+
+    public static void downloadGoodsDetail(Context context, int goodsId, OkHttpUtils.OnCompleteListener<GoodsDetailsBean> listener) {
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
+                .addParam(I.GoodsDetails.KEY_GOODS_ID, String.valueOf(goodsId))
+                .targetClass(GoodsDetailsBean.class)
+                .execute(listener);
+    }
+}
